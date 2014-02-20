a93777adfa4815f245b1389483d51836e303e3c5
Lydia <lydiawallgren@gmail.com>
2014-02-20 16:13:06 +0100

    added css file + moved css for home to the css file

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -2,24 +2,6 @@
 var HomeView = function (container,model) {
 	
 	
-	
-	var body = document.body;
-	body.style.backgroundImage = 'url(../dinnerplanner-html/images/Food-Image.jpg)';
-	body.style.backgroundSize = "100%";
-
-	var homeHeader = document.getElementById('homeHeader');
-	homeHeader.style.background = "#FACE78";
-	homeHeader.style.opacity = '0.6';
-	homeHeader.style.paddingLeft = '5%';
-
-	var homeView = document.getElementById('homeView');
-	homeView.style.background = "white";
-	homeView.style.opacity = '0.6';
-	homeView.style.position = 'absolute';
-	homeView.style.top = '25%';
-	homeView.style.paddingLeft = '5%';
-	homeView.style.paddingBottom = '2%';
-
 	//we set the constant text
 	
 	var homeViewH2 = document.getElementById('homeViewH2');
